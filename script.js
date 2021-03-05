6104d4067cc569e2f2f537286929e606deac51ae
Anna-fog <anna49.fog@yandex.ru>
2021-03-05 11:33:59 +0300

    feat: implement reset function

--- script.js
+++ script.js
@@ -2,9 +2,10 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     const filters = document.querySelector('.filters');
-    const btnReset = document.querySelector('.btn-reset');
     const inputs = document.querySelectorAll('input');
     const outputs = document.querySelectorAll('output');
+    const buttons = document.querySelectorAll('.btn');
+    const btnReset = document.querySelector('.btn-reset');
     const btnNext = document.querySelector('.btn-next');
 
     function handleUpdate(e) {
@@ -13,15 +14,23 @@ document.addEventListener('DOMContentLoaded', () => {
         e.target.nextElementSibling.value = e.target.value;
     }
 
-    function resetFilters() {
+    function resetFilters(e) {
+        addActiveClass(e);
         inputs.forEach(input => {
             input.value = input.defaultValue;
             document.documentElement.style.setProperty(`--${input.name}`,`${input.value}` + input.dataset.sizing);
             outputs.forEach(output => {
-                output.value = input.value
+                output.value = '0';
             });
         });
-     }
+    }
+
+    function addActiveClass(e) {
+        buttons.forEach(btn => {
+            btn.classList.remove('btn-active');
+        });
+        e.target.classList.add('btn-active');
+    }
 
     filters.addEventListener('input', (e) => {
         if (e.target.type === 'range') {
